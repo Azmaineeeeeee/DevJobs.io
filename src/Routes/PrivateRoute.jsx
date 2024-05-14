673c13503068365668500a37d40ac13f6b2e8659
Azmaine Mushfiq Fatin <azmainemushfiq@gmail.com>
2024-05-15 01:14:21 +0600

    Private Pages

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -1,25 +1,32 @@
-import { useContext } from "react";
-
-import { useLocation } from "react-router-dom";
-import PropTypes from "prop-types";
-import { Navigate } from "react-router-dom";
-import { Context } from "../Providers/AuthProviders";
+import { useContext, useState } from 'react';
+import LoginModal from '../Components/LoginModal'; 
+import { Context } from '../Providers/AuthProviders';
 
 const PrivateRoute = ({ children }) => {
   const { user, loading } = useContext(Context);
-  let location = useLocation();
+  const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
+ 
 
-  if (loading) {
-    return <span className="loading loading- loading-lg flex justify-center items-center h-screen"></span>;
-  }
-
-  if (user) {
+  if(user) {
     return children;
   }
-  return <Navigate state={location.pathname} to="/login"></Navigate>;
+
+  if (loading) {
+    return <span className="loading loading-loading-lg flex justify-center items-center h-screen"></span>;
+  }
+
+  if (!user) {
+    return (
+      <>
+        <LoginModal open={isLoginModalOpen} onClose={() => setIsLoginModalOpen(false)} />
+        
+      </>
+    );
+  }
+
+  return children;
 };
-PrivateRoute.propTypes = {
-  children: PropTypes.node,
-};
+
+
 
 export default PrivateRoute;
