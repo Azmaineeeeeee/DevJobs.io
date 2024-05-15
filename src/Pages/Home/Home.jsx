d4a623904027b9c72623d61991eae287d5cb497b
Azmaine Mushfiq Fatin <azmainemushfiq@gmail.com>
2024-05-15 16:05:26 +0600

    Home Section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,6 +2,8 @@ import BannerHome from "../../Components/BannerHome";
 import JobTabs from "../../Components/JobTabs/JobTabs";
 import CountUpCards from "../../Components/CountUpCard"
 import BlogsPage from "../BlogsPage";
+import TimeLine from "../../Components/TimeLine";
+import image from "../../../public/b157fb9f9246b335ff09562abd2d478e.png"
 
 
 const Home = () => {
@@ -16,10 +18,22 @@ const Home = () => {
             <div className="text-center p-4 mt-2">
             <JobTabs></JobTabs>
             </div>
+            
+            <h1 className="text-center text-3xl font-bold mt-4 p-6">How Does DevJobs.io Work?</h1> 
+          <p className="text-center text-sm">Post a job to tell us about your project. We'll quickly match you with the right job seekers.</p> 
+            <div className="p-4 mt-2 flex lg:flex-row flex-col justify-between items-center">
+            <div>
+            <TimeLine></TimeLine>
+            </div>
+            <div>
+                <img src={image}></img>
+            </div>
+            </div>
 
             <div className="text-center p-4 mt-2">
             <BlogsPage></BlogsPage>
             </div>
+            
            
         </div>
     );
